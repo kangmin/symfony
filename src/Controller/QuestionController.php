87b53d789aefc4748face4b686be3e63d8075529
Kanz Eng <kangminz@yahoo.com>
2020-04-01 07:33:01 -0700

    Add the twig.

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
 
     /**
@@ -20,7 +21,20 @@ class QuestionController
      * @Route("/questions/{wildcard}")
      */
     public function show($wildcard){
+        $answers = [
+            "That is not bab idea :-)",
+            "It depends on whether you are in sociology or psychology",
+            "Most of the population believe that paranoid is either derogertory or pathological.",
+        ];
+        return $this->render("question/show.html.twig", [
+            'question' => ucwords(str_replace("-",' ', $wildcard )),
+            'answers' => $answers,
+
+        ]);
+
+        /*
         return new Response( sprintf('Here will be the question of "%s" 
         and its answers.', ucwords(str_replace("-",' ', $wildcard ))));
+       */
     }
 }
